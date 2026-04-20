8d359dd8e75d50c56a9700d66b5a0b204508955d
Aliyu Abubakar <aliyufari@gmail.com>
2026-04-20 21:25:55 +0100

    Added User Seeder

---
+++ src/main/java/com/afgicafe/flight/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.afgicafe.flight.controller;
+
+import com.afgicafe.flight.utils.ApiResponse;
+import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Hidden
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public ResponseEntity<ApiResponse<?>> index () {
+        return ResponseEntity.ok(
+                ApiResponse.ok("Blink Flight API")
+        );
+    }
+
+
+}
